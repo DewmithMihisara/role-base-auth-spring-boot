bf86bf70221ff30c005ada3c978722c5afa13dca
Dewmith Mihisara <124574193+DewmithMihisara@users.noreply.github.com>
2025-01-28 15:59:58 +0530

    init project

---
+++ src/test/java/com/hcodesolutions/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcodesolutions.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
